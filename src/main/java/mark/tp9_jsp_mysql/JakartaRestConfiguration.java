3faa0a072e6703421a055c96cca876d0f6fe1fa9
John Hughes <161081106+HughesJohnFranco@users.noreply.github.com>
2024-12-04 23:11:26 -0300

    Update JakartaRestConfiguration.java

--- src/main/java/mark/tp9_jsp_mysql/JakartaRestConfiguration.java
+++ src/main/java/mark/tp9_jsp_mysql/JakartaRestConfiguration.java
@@ -3,10 +3,6 @@ package mark.tp9_jsp_mysql;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Configures Jakarta RESTful Web Services for the application.
- * @author Juneau
- */
 @ApplicationPath("resources")
 public class JakartaRestConfiguration extends Application {
     
