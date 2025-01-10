6be8d04098b832882e353986ac0cbcc397520644
John Hughes <161081106+HughesJohnFranco@users.noreply.github.com>
2025-01-10 20:55:43 -0300

    actualizacion_cotizacion.js

--- src/main/java/cotizacion.js
+++ src/main/java/cotizacion.js
@@ -1,9 +1,7 @@
 function mostrarCotizacion() {
     var idVehiculo = document.getElementById("idid").value;
     var diasAlquiler = document.getElementById("dias").value;
-
     if (idVehiculo && diasAlquiler > 0) {
-
         window.location.href = "cotizacion.jsp?id_vehiculo=" + idVehiculo + "&dias_alquiler=" + diasAlquiler;
     } else {
         alert("Por favor, seleccione un vehículo y un número válido de días.");
